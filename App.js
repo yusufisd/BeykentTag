5a690510f38b8559d0e36c6904f9f3b7765ac654
yusuf ishak damar <ysfishak14@gmail.com>
2024-04-03 00:22:57 +0300

    okey

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import * as React from 'react';
-import 'react-native-gesture-handler';
 import Services from './src/pages/Services';
 import Login from './src/pages/Login';
 import Register from './src/pages/Register';
 import FirstPage from './src/pages/FirstPage';
 import Detail from './src/pages/Detail';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { store } from './src/redux/store';
 import { Provider } from 'react-redux';
 
-const Stack = createNativeStackNavigator();
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
 const App = () => {
 return (
 <Provider store={store}>
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Sürücü Ol/Yolcu Ol" component={FirstPage} />
-      <Stack.Screen name="Giriş Yap" component={Login} />
-      <Stack.Screen name="Servisler" component={Services} />
-      <Stack.Screen name="İçerik Detay" component={Detail} />
-      <Stack.Screen name="Kayıt Ol" component={Register} />
-
-    </Stack.Navigator>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Sürücü Ol/Yolcu Ol" component={FirstPage} />
+      <Drawer.Screen name="Giriş Yap" component={Login} />
+      <Drawer.Screen name="Servisler" component={Services} />
+      <Drawer.Screen name="İçerik Detay" component={Detail} />
+      <Drawer.Screen name="Kayıt Ol" component={Register} />
+    </Drawer.Navigator>
   </NavigationContainer>
 </Provider>
 
