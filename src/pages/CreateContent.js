566bad6e37b32fe984bf3bb82edea12d6c19267d
Abdullah Baykal <97851565+abdullahbykl61@users.noreply.github.com>
2024-05-26 17:56:58 +0300

    database

--- src/pages/CreateContent.js
+++ src/pages/CreateContent.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, Button, StyleSheet, SafeAreaView, Image, TextInput,ScrollView } from "react-native";
 import { useSelector, useDispatch } from 'react-redux'
 import { register } from '../redux/userSlice';
+import firestore from '@react-native-firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCHc6R-pLjxqK9Wq2r2NXrUPqAMp70ZHM",
+  authDomain: "beykenttag.firebaseapp.com",
+  projectId: "beykenttag",
+  storageBucket: "beykenttag.appspot.com",
+  messagingSenderId: "116644081560",
+  appId: "1:116644081560:web:22c5c9da8b28803dd845ed",
+  measurementId: "G-4FWDS63WK7"
+};
 
 const CreateContent = () => {
+  const [from,setFrom] = useState('')
+  const [to,setTo] = useState('')
+  const [price,setPrice] = useState('')
+  const [capacity,setCapacity] = useState('')
+  const [date,setDate] = useState('')
+  const [time,setTime] = useState('')
+
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
+
+  const addService = async (from, to, price, capacity, date, time) => {
+    try {
+      await addDoc(collection(db, 'Services'), {
+        from: from,
+        to: to,
+        price: price,
+        capacity: capacity,
+        date: date,
+        time: time,
+        createdAt: new Date()
+      });
+      console.log('Hizmet başarıyla eklendi!');
+    } catch (e) {
+      console.error('Hizmet eklenirken hata oluştu: ', e);
+    }
+  };
+
+  const handleSubmit = () => {
+    addService(from, to, price, capacity, date, time);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.card}>
           <Text style={styles.card_title}>Servis Ekle</Text>
-          <TextInput placeholder='Nereden' style={styles.input}></TextInput>
-          <TextInput placeholder='Nereye' style={styles.input}></TextInput>
-          <TextInput placeholder='Tarih' style={styles.input}></TextInput>
-          <TextInput placeholder='Saat' style={styles.input} ></TextInput>
-          <TextInput placeholder='Kapasite' style={styles.input} ></TextInput>
-          <TextInput placeholder='Fiyat' style={styles.input} ></TextInput>
-          <Button  title='kaydet' ></Button>
+          <TextInput placeholder='Nereden' style={styles.input} onChangeText={text => setFrom(text)} ></TextInput>
+          <TextInput placeholder='Nereye' style={styles.input} onChangeText={text => setTo(text)}></TextInput>
+          <TextInput placeholder='Tarih' style={styles.input} onChangeText={text => setDate(text)}></TextInput>
+          <TextInput placeholder='Saat' style={styles.input} onChangeText={text => setTime(text)}></TextInput>
+          <TextInput placeholder='Kapasite' style={styles.input} onChangeText={text => setCapacity(text)}></TextInput>
+          <TextInput placeholder='Fiyat' style={styles.input} onChangeText={text => setPrice(text)}></TextInput>
+          <Button  title='kaydet' onPress={handleSubmit} ></Button>
         </View>
     </ScrollView>
 </SafeAreaView>
